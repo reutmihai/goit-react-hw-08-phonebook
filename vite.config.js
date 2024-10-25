af545521a6c523e128758448c1a1b2b969180d8c
Reut Mihai <reutmihai@icloud.com>
2024-10-25 19:28:56 +0300

    update package.json

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Template-react/',
+  base: "/goit-react-hw-04-phonebook/",
   plugins: [react()],
-})
+});
