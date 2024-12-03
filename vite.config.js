6e7098476567bce536e20eb608115fa789f1086e
Reut Mihai <reutmihai@icloud.com>
2024-12-03 17:50:07 +0200

    fixing live page

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/goit-react-hw-08-phonebook/",
+  base: "/goit-react-hw-08-phonebook/",
   plugins: [react()],
 });
